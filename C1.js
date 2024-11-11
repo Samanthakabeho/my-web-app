fd46f6e2b3a6671efce9699bed1d71e5638fe176
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-11 21:15:12 +0300

    Add files via upload

---
+++ C1.js
@@ -0,0 +1,12 @@
+//NUMBER 1
+//Determining if the discount is applicable
+let first_product = 20000;
+let second_product = 30000;
+let third_product = 35000;
+let total_price = first_product + second_product + third_product;
+if (total_price > 100000) {
+    console.log("Discount applicable");
+}else {
+    console.log("No Discount");
+}
+
