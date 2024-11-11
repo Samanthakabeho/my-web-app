366f939bf3148b4dc9c147cff1c789280c706248
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-11 21:19:56 +0300

    Add files via upload

---
+++ D 1.js
@@ -0,0 +1,16 @@
+//Number 1
+//Program using a switch statement
+let networkcode =2;
+switch(networkcode){
+    case 1:
+        console.log("MTN vall rate is 10 UGX per second");
+        break;
+        case 2:
+            console.log("Airtel call rate is 8 UGX per second");
+            break;
+            case 3:
+                console.log("Africell call rate is 9 UGX per second");
+                break;
+                default:
+                    console.log("Invalid networkcode");
+}
