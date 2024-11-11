bad00a72421dfd2e427e65e815ca4adf6e14f645
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-11 13:06:01 +0300

    Add files via upload

---
+++ B2.js
@@ -0,0 +1,7 @@
+//STEPS USED TO INSTALL JAVASCRIPT.
+// Download and install visual studio code
+//Go to the visual studio code website
+//Download the installer of your operating system forexample (windows)
+//Install visual studio code following the  instructions
+//Download  Node.js and install it on your mavhine
+//Install javascript from your visual studio code
