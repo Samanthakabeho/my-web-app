367afe7b66fe9bff622b3859902199c675cbb0bb
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-11 21:16:31 +0300

    Add files via upload

---
+++ C2.js
@@ -0,0 +1,8 @@
+
+// Aprogramm that stores the students UACE
+let UACEpoints =10;
+if (UACEpoints > 7){
+    console.log("Eligible for BSIT and Datascience");
+}else{
+    console.log("choose social science")
+}
