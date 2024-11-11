5d6c16638bb8eb8e6640783920cdf337eba30fc6
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-11 11:19:05 +0300

    Add files via upload

---
+++ A3.js
@@ -0,0 +1,7 @@
+
+//Number 3
+// Var is a fuction that can be redeclared and reassigned
+//let can be reassigned but cannot be redeclared 
+//const can neither be redeclared nor reassigned
+//Reassigning a variable with let is allowed but only when it is with in the scope
+//And trying to reassign a const will result in to an error
