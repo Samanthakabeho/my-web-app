b5a944191eaac56049106467647d4fc6fea266b2
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-11 21:17:26 +0300

    Add files via upload

---
+++ E1.js
@@ -0,0 +1,7 @@
+
+// Programm that prints the rainfall recorded in millimeters
+
+let rainfalldata =[12,15,10,20,13,14,7,18,6,9];
+for (let k = 0; k < rainfalldata.length; k++){
+    console.log(`Day ${k +1}: ${rainfalldata[k]}mm`);
+}
