19e0cd5b485120e54132243a3a1e491b4f1830a9
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-11 11:17:54 +0300

    Add files via upload

---
+++ A2.js
@@ -0,0 +1,6 @@
+
+//storing the name of a country in Africa and it's population
+const current_year =2024;
+let country = "Ghana";
+let population = 2000;
+console.log(`country:${country},population:${population},year:${current_year}`);
